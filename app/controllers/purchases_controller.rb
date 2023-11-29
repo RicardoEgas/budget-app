41a8e721e1e459fd13feee68b39dd2dbbeeb4a37
RicardoEgas <rj_egas@hotmail.com>
2023-11-29 17:15:53 +0100

    Fix transaction issue

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,38 @@
+class PurchasesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_group
+  
+    def index
+      @purchases = @group.purchases.order(created_at: :desc)
+      @total_amount = @purchases.sum(:amount)
+    end
+  
+    def new
+      @purchase = @group.purchases.build
+    end
+  
+    def create
+        @purchase = @group.purchases.new(purchase_params)
+        @purchase.user = current_user
+    
+        respond_to do |format|
+          if @purchase.save
+            format.html { redirect_to group_purchases_path(@group), notice: 'Transaction was successfully created.' }
+          else
+            format.html { render :new }
+            format.turbo_stream { render turbo_stream: turbo_stream.replace('new_purchase', partial: 'purchases/form', locals: { purchase: @purchase }) }
+          end
+        end
+      end
+  
+    private
+  
+    def set_group
+      @group = current_user.groups.find(params[:group_id])
+    end
+  
+    def purchase_params
+      params.require(:purchase).permit(:name, :amount)
+    end
+  end
+  
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+    def sign_out_user
+      sign_out(current_user)
+      redirect_to root_path
+    end
+  end
+  
