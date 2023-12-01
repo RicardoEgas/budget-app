d1c774380a79823d9f1e5f269669c797cca734b5
RicardoEgas <rj_egas@hotmail.com>
2023-12-01 13:11:30 +0100

    Update secret key base

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module BudgetApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.edit_encrypted_file = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
